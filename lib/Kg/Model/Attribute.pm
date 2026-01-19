6cd4af213c146231e538bc0cbb29a78ad8b56519
Brad Clawsie <brad@b7j0c.org>
2026-01-19 11:23:03 -0600

    complete Org pre-db model

--- lib/Kg/Model/Attribute.pm
+++ lib/Kg/Model/Attribute.pm
@@ -7,6 +7,18 @@ use Readonly ();
 our $VERSION   = '0.0.1';
 our $AUTHORITY = 'cpan:bclawsie';
 
+Readonly::Scalar our $DATE => '%Y-%m-%d %H:%M:%S GMT';
+
+Readonly::Scalar our $DATE_RE => qr{^
+  \d{4}-
+  \d{2}-
+  \d{2}\s
+  \d{2}:
+  \d{2}:
+  \d{2}\s
+  GMT
+  $}x;
+
 Readonly::Scalar our $ROLE_NORMAL        => 1;
 Readonly::Scalar our $ROLE_ADMIN         => 2;
 Readonly::Scalar our $ROLE_TEST          => 3;
@@ -15,10 +27,10 @@ Readonly::Scalar our $STATUS_ACTIVE      => 2;
 Readonly::Scalar our $STATUS_INACTIVE    => 3;
 
 our @EXPORT_OK =
-  qw( $ROLE_NORMAL $ROLE_ADMIN $ROLE_TEST $STATUS_UNCONFIRMED $STATUS_ACTIVE $STATUS_INACTIVE );
+  qw( $DATE $DATE_RE $ROLE_NORMAL $ROLE_ADMIN $ROLE_TEST $STATUS_UNCONFIRMED $STATUS_ACTIVE $STATUS_INACTIVE );
 our %EXPORT_TAGS = (
   all => [
-    qw( $ROLE_NORMAL $ROLE_ADMIN $ROLE_TEST $STATUS_UNCONFIRMED $STATUS_ACTIVE $STATUS_INACTIVE )
+    qw( $DATE DATE_RE $ROLE_NORMAL $ROLE_ADMIN $ROLE_TEST $STATUS_UNCONFIRMED $STATUS_ACTIVE $STATUS_INACTIVE )
   ]
 );
 
